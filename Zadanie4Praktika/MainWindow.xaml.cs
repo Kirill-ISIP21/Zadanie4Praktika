aa2b52879126dfc7f067e778576750183e992d06
k.chukharev@mail.ru <k.chukharev@mail.ru>
2020-12-24 02:08:59 +0300

    Добавьте файлы проекта.

---
+++ Zadanie4Praktika/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie4Praktika
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Class1 sl = new Class1();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            sl.slovo = txtSl.Text;
+            int n = sl.slovo.Length;
+            for (int i = 0; i < n; i++)
+            {
+
+                sl.slovo = sl.slovo.Insert(n, "!");
+
+            }
+            newSlovo.Content = sl.slovo;
+        }
+    }
+}
